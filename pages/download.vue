79f16b08b99adb7d739360ef82f6c8083a69f6ea
Janakan-S <janakans@protonmail.com>
2024-11-22 16:21:31 -0500

    Add download templates

---
+++ pages/download.vue
@@ -0,0 +1,23 @@
+<script setup>
+
+</script>
+
+<template>
+    <main class="">
+        <section class="bg-[#ABD7E0] py-8 px-2">
+            <div class="max-w-7xl mx-auto">
+                <Breadcrumbs :crumbs="[]" />
+                <h1 class="text-[#665E7A] text- text-4xl font-bold mt-4">
+                    Download
+                </h1>
+                <div class="flex justify-between flex-wrap text-gray-700">
+                    <div class="max-w-prose mt-4">
+                        <p class="max-w-prose">
+                            Export dataset based on selection criteria
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </main>
+</template>
